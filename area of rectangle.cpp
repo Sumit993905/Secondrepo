3d9c5459deb21ca35ac4750060a1fc73cc60c808
Sumitraj <officialsumitraj993905@gmail.com>
2023-09-03 11:03:33 +0530

    added

---
+++ area of rectangle.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+ int l,b;
+ 
+ cout<<" ENTER THE VALUE OF LENGTH: "<<endl;
+ cin>>l;
+ cout<<" ENTER THE VALUE OF BREADTH : "<<endl;
+ cin>>b;
+ cout<<" AREA OF RECTANGLE IS :"<<l*b<<endl;
+
+ return 0;
+} 
+
+
+
+
+
